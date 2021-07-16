4acea3722dc3bb793f50140ea4728e1d4a3504e0
martin0641 <martin0641@gmail.com>
2021-07-16 16:45:55 -0400

    Create aws-launch-template-describe.ps1

---
+++ aws-launch-template-describe.ps1
@@ -0,0 +1,8 @@
+$describetemplate = "aws ec2 describe-launch-templates"
+$terminate = "aws ec2 Remove-EC2Instance -InstanceId i-06fab3aef78acf0cf -Force"
+
+#Invoke-Expression $Command
+ 
+#aws ec2 run-instances --launch-template LaunchTemplateId=lt-0a7bab162b38d866f
+
+Invoke-Expression $describetemplate
