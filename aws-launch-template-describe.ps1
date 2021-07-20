badb370d1c4a07a15fd5f334dec7944c2d037414
martin0641 <martin0641@gmail.com>
2021-07-19 22:40:36 -0400

    Update aws-launch-template-describe.ps1

--- aws-launch-template-describe.ps1
+++ aws-launch-template-describe.ps1
@@ -6,3 +6,11 @@ $terminate = "aws ec2 Remove-EC2Instance -InstanceId i-06fab3aef78acf0cf -Force"
 #aws ec2 run-instances --launch-template LaunchTemplateId=lt-0a7bab162b38d866f
 
 Invoke-Expression $describetemplate
+
+
+ConvertFrom-Json
+                [-InputObject] <String>
+                [-AsHashtable]
+                [-Depth <Int32>]
+                [-NoEnumerate]
+                [<CommonParameters>]
